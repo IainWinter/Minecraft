5900b631e0e9d582828ba9bca709b6b30c4a58a8
Iain Winter <iainwinter1@gmail.com>
2018-09-18 22:42:07 -0400

    almost there

--- Space/include/ecs/component_view.h
+++ Space/include/ecs/component_view.h
@@ -6,32 +6,21 @@
 template<typename... ComponentsT>
 class component_view {
 private:
-	std::tuple<component_array<ComponentsT>...> m_components;
-	unsigned int m_count;
+	struct ssss {
+		std::tuple<component_array<ComponentsT>::iterator...> components;
+		unsigned int m_count;
+	};
 public:
-	component_view(component_array<ComponentsT>... components) {
-		m_components = std::make_tuple<ComponentsT*...>(components->get_components()); 
+	component_view(component_array<ComponentsT>*... components) {
+		component_array<ComponentsT>::iterator
 
 
-
-		component_array<int> d = component_array<int>();
-		d.add_component(0, 1);
-		d.add_component(4, 123);
-		component_array<int>::iterator itr = d.begin();
-		int i = *itr;
-		itr++;
-		int ii = *itr;
-		bool isEnd = itr == d.end();
-		itr++;
-		int iii = *itr;
+		ccomponents = std::make_tuple<component_array<ComponentsT>::iterator...>(
+			components->begin()...
+		);
 	}
 
 	~component_view() {}
 
-	//std::tuple<ComponentTs&...> operator[](unsigned int index);
-};
 
-//template<typename... ComponentTs>
-//std::tuple<ComponentTs&...> component_view<...ComponentTs>::operator[](unsigned int index) {
-//	
-//}
+};
